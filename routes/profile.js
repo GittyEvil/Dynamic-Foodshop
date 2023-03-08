cc7d77dcb2b3d2028a010e87a696857c90735a4a
Your Name <you@example.com>
2023-03-08 14:17:42 +0100

    s

--- routes/profile.js
+++ routes/profile.js
@@ -37,16 +37,28 @@ router.post('/',function(req,res,next) {
     for(var i=0; i< users; i++) {
         if(req.session.username == users[i].username && users[i].password == req.session.password
            && users[i].gmail == req.session.gmail){
+
           var oldmail = users[i].gmail;
           var oldusername = users[i].gmail;
+
+          req.body.username = null
+          req.body.username = null
+
+
+          //behöver fixa något som byter ut den gamla infon och ger den nya
           var NewUserInformation = {
             username:req.body.username,
             password:req.body.password,
             email:req.body.email
         }
         
+          users[i].push(NewUserInformation)
 
-    
+
+          let uppdateraInfo = JSON.stringify(users[i])
+
+          fs.writeFileSync('user.json', uppdateraInfo)
+        res.render('profile')
       }
     }
     
